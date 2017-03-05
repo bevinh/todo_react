95002c25e157008ea598c69e29dd673d8a42a46e
bevinh <bevin.hernandez@gmail.com>
2017-03-05 18:27:50 -0500

    Added changes to router

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -25,7 +25,13 @@ export class Router extends Component {
             linkHandler: this.handleLinkClick
         }
     }
-
+    componentDidMount(){
+        window.onpopstate = () => {
+            this.setState({
+                route: getCurrentPath()
+            })
+        }
+    }
     render() {
         return (
             <div>
